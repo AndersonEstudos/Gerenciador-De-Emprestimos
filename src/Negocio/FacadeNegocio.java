46c537d26c811a7f27a63df53fc2e2e7b36ef4d7
Anderson Araujo <anderson@C14CR01.(none)>
2015-06-05 14:11:10 -0300

    outra atualização

--- src/Negocio/FacadeNegocio.java
+++ src/Negocio/FacadeNegocio.java
@@ -1,5 +1,11 @@
 package Negocio;
 
 public class FacadeNegocio {
+	
+	public void CadastrarUsuario(){}
+	public void CadastrarProprietario(){}
+	public void SolicitarEmprestimo(){}
+	public void CancelarEmprestimo(){}
+	
 
 }
